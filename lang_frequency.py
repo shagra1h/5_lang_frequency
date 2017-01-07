55f875a2c782e4ebf9721e91a345886a5d0db0b8
shagra1h <achkael.serg@yandex.ru>
2017-01-07 20:56:15 +0300

    Частотный анализ слов текста

--- lang_frequency.py
+++ lang_frequency.py
@@ -2,26 +2,21 @@ import re
 import operator
 import sys
 import os.path
+from collections import Counter
 
 
 def load_data(filepath):
     if os.path.exists(filepath):
         with open(filepath, "r") as input_file:
             filedata = input_file.read()
-            filedata = filedata.strip()
+            filedata = filedata.lower().strip()
             return filedata
 
 
 def get_most_frequent_words(text):
     words_in_text = re.findall(r'[\w]+', text)
-    words_frequency = {}
-    for word in words_in_text:
-        word = word.lower()
-        if word in words_frequency.keys():
-            words_frequency[word] += 1
-        else:
-            words_frequency[word] = 1
-    return sorted(words_frequency.items(), key=operator.itemgetter(1), reverse=True)
+    words_frequency = Counter(words_in_text)
+    return words_frequency.most_common(10)
 
 
 if __name__ == '__main__':
@@ -36,7 +31,5 @@ if __name__ == '__main__':
         print("Incorrect path to the file!")
         sys.exit(0)
     lang_frequency = get_most_frequent_words(data_from_file)
-    for iterator, word in enumerate(lang_frequency):
+    for word in lang_frequency:
         print("Word '%s' repeated in the text %s times" % (word[0], word[1]))
-        if iterator >= 9:
-            break
